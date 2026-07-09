918c6dd61fb44d2d59564b4feac051712fbb82cf
CuneytOrhanVarol <cuneytorhanvarol@gmail.com>
2026-07-09 16:11:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 import streamlit as st
+
 from database import veritabani_hazirla
 from login import giris_ekrani
 from operasyon import operasyon_paneli
 from aktarim import kayit_ekle_aktar
 from temizlik import hatali_kayit_temizligi, log_ekrani
 
-st.set_page_config(page_title="FB Operasyon Merkezi", layout="wide")
+
+st.set_page_config(
+    page_title="FB Operasyon Merkezi",
+    layout="wide"
+)
 
 veritabani_hazirla()
+
 
 if "kullanici" not in st.session_state:
     giris_ekrani()
     st.stop()
 
+
 mevcut_user = st.session_state["kullanici"]
 yetki = st.session_state["yetki"]
 
+
 st.sidebar.title(f"👤 {mevcut_user}")
 st.sidebar.caption(f"Yetki: {yetki}")
+
 
 menu = ["📦 Operasyon Paneli"]
 
@@ -26,7 +35,9 @@ if yetki == "Yönetici":
     menu.append("🧹 Hatalı Kayıt Temizliği")
     menu.append("🕒 Log")
 
+
 secim = st.sidebar.radio("Menü", menu)
+
 
 if secim == "📦 Operasyon Paneli":
     operasyon_paneli(mevcut_user, yetki)
@@ -39,6 +50,7 @@ elif secim == "🧹 Hatalı Kayıt Temizliği":
 
 elif secim == "🕒 Log":
     log_ekrani()
+
 
 if st.sidebar.button("🚪 Çıkış"):
     st.session_state.clear()
